9755795c7d7de6dff7ed56c5d3ea7efe474569fa
LVTruongCa <52068034+LVTruongCa@users.noreply.github.com>
2019-11-11 16:12:22 +0700

    Update source code

---
+++ Repository/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repositorys
+{
+  public interface IRepository<TEntity> where TEntity:class
+    {
+
+    }
+}
---
+++ Repository/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repositorys
+{
+   public interface IUnitOfWork: IDisposable
+   {
+       IRepository<T> GetRepository<T>() where T : class;
+       void Save();
+       void Dispose();
+   }
+}
